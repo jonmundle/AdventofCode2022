041de745e90677c32f60247a424f645abee5c946
Jon Mundle <jonmundle@gmail.com>
2022-12-01 22:40:52 -0600

    Part 2, write to host

--- Day1/Invoke-ACSolution.ps1
+++ Day1/Invoke-ACSolution.ps1
@@ -4,5 +4,7 @@ $ElfCalories = foreach ($Elf in $Elves) {
     $CalorieList = $Elf -split "`r`n"
     ($CalorieList | Measure-Object -Sum).Sum
 }
-$MaxCalories = $ElfCalories | Measure-Object -Maximum
-return $MaxCalories.Maximum
+$MaxCalories = ($ElfCalories | Measure-Object -Maximum).Maximum
+Write-Host "The maximum calories is $MaxCalories"
+$Top3Sum = ($ElfCalories | Sort-Object -Descending | Select-Object -First 3 | Measure-Object -Sum).Sum
+Write-Host "The sum of the top 3 is $Top3Sum"
